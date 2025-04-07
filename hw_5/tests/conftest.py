a7b72309b360bc07f4a2395df619708e5b149b18
Azamat-V-U <tester_01_980@mail.ru>
2025-04-07 12:37:41 +0300

    added headless mode

--- hw_5/tests/conftest.py
+++ hw_5/tests/conftest.py
@@ -15,6 +15,11 @@ def pytest_addoption(parser):
         help="This is the browser configuration"
     )
     parser.addoption(
+        "--headless",
+        action="store_true",
+        help="This is the headless configuration"
+    )
+    parser.addoption(
         "--maximize",
         action="store_true",
         help="This is maximize window configuration"
@@ -29,15 +34,22 @@ def base_url(request):
 @pytest.fixture
 def browser(request):
     browser_name = request.config.getoption("--browser")
+    headless = request.config.getoption("--headless")
     maximize = request.config.getoption("--maximize")
     if browser_name == "chrome":
         options = ChromeOptions()
+        if headless:
+            options.add_argument("--headless")
         driver = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         options = FirefoxOptions()
+        if headless:
+            options.add_argument("--headless")
         driver = webdriver.Firefox(options=options)
     elif browser_name == "edge":
         options = EdgeOptions()
+        if headless:
+            options.add_argument("--headless")
         driver = webdriver.Edge(options=options)
     else:
         raise ValueError("Browser name must be either 'chrome', 'firefox' or 'edge'")
